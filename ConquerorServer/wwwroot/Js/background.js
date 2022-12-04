581e73f7b923d2973cf2f46670e4cf36bb0e546b
mariansusanaas@gmail.com <Marian>
2022-12-04 18:32:21 -0500

    .

--- ConquerorServer/wwwroot/Js/background.js
+++ ConquerorServer/wwwroot/Js/background.js
@@ -1,25 +1,7 @@
-function CreateCardBG() {
-  const body = document.querySelector("body")
 
-  body.style.background = "url(../images/cards.jpg)";
-}
 
 function SelectPlayerBG() {
   const body = document.querySelector("body")
 
   body.style.background = "url(../images/player.jpg)";
-}
-
-function IndexBg() {
-  const body = document.querySelector("body")
-
-  body.style.background = "url(../images/main-menu.jpg)";
-}
-
-
-function GameEnvironmentBg() {
-  const body = document.querySelector("body")
-  body.style.background = "url(../images/fondo7.jpeg)";
-  body.style.backgroundSize = "cover";
-
 }
