795cb35c4669380dab42360c3dc9b4c009d343e1
mariansusanaas@gmail.com <Marian>
2023-01-03 13:12:23 -0500

    graphics updated

--- ConquerorServer/wwwroot/Js/background.js
+++ ConquerorServer/wwwroot/Js/background.js
@@ -1,6 +1,6 @@
 function GameEnvironmentBG() {
   const body = document.querySelector("body")
-  body.style.background = "url(../images/fondo1.jpg)";
+  body.style.background = "url(../images/fondo1.jpeg)";
   body.style.backgroundSize = "cover";
   body.style.backgroundRepeat = "no repeat";
 }
